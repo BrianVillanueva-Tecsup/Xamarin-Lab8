c0e8992b14672a612679ad41057c6774d21a7dae
abc <abc@gmail.com>
2020-05-19 19:31:38 -0500

    First commit

---
+++ Ejercicio09/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var buttonStyle = new Style(typeof(Button))
+            {
+                Setters = { new Setter { Property = Button.TextColorProperty,    Value = Color.Teal }
+            }
+            };
+            //Resources = new ResourceDictionary();
+            Resources.Add("buttonStyleCode", buttonStyle);
+
+            MainPage = new NavigationPage(new Login());
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
